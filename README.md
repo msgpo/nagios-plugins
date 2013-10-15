957a9da3735da0dd2ef65cf3fe8649a14aa45380
Alex Williams <awilliams@alexwilliams.ca>
2013-10-15 12:56:50 +0000

    Add check_procs2 wrapper to get perf data from check_procs

--- README.md
+++ README.md
@@ -2,4 +2,5 @@
 
 * plugins/check_connections.pl - slightly modified to output perfdata
 * plugins/check_mem.pl - unmodified, monitors caches too
+* plugins/check_procs2.pl - slightly modified procs wrapper with perf data
 
---
+++ plugins/check_procs2.pl
@@ -0,0 +1,30 @@
+#!/usr/bin/perl
+
+use strict;
+use File::Basename;
+
+# build new command string
+my $cmd = dirname(__FILE__) . "/check_procs";
+for (my $i = 0; $i <=  $#ARGV; $i++) {
+        $cmd .= " " . $ARGV[$i];
+}
+
+# get the return data
+my $stdout = `$cmd`;
+my $exitCode = $?;
+
+# add perfdata
+my $state = "*", my $procs = 0;
+chop $stdout;
+if($stdout =~ m/PROCS\s.*:\s(\d+)\sprocesses/) {
+        $procs = $1;
+}
+
+if($stdout =~ m/PROCS\s.*:\s(\d+)\s.*STATE\s=\s(.*)$/) {
+        $procs = $1;
+        $state = $2;
+}
+# return new data
+print "$stdout|state=$state;procs=$procs;\n";
+exit ($exitCode);
+
